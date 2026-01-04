07fa74778f5b6bd2e6ea44ec3766cbdcc94bc6ba
jr9 <jr9phil@gmail.com>
2026-01-04 16:20:54 -0600

    react routes with blank pages

---
+++ frontend/src/pages/Create.jsx
@@ -0,0 +1,7 @@
+const CreatePage = () => {
+    return <div>
+        <h1>Create</h1>
+    </div>
+};
+
+export default CreatePage;
---
+++ frontend/src/pages/Details.jsx
@@ -0,0 +1,9 @@
+const DetailPage = () => {
+    return (
+        <div>
+            <h1>Details</h1>
+        </div>
+    );
+};
+
+export default DetailPage;
