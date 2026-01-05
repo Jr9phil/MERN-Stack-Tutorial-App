fd4dd01002d6742aaa3413cb353b31ec1f6b6d73
jr9 <jr9phil@gmail.com>
2026-01-05 16:05:20 -0600

    update functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import RateLimitedUI from "../components/RateLimited";
 import { useEffect, useState } from "react";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
+import { LoaderIcon } from "lucide-react";
 
 const HomePage = () => {
     const [rateLimited, setRateLimited] = useState(false);
@@ -38,7 +39,9 @@ const HomePage = () => {
         <Navbar />
         {rateLimited && <RateLimitedUI />}
         <div className="max-w-7xl mx-auto p-4 mt-6">
-            {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+            {loading && <div className="min-h-screen bg-base-200 flex items-center justify-center">
+            <LoaderIcon className="animate-spin size-10" />
+        </div>}
 
             {notes.length > 0 && !rateLimited && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
